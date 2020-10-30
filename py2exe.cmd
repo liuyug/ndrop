0711510a1c502b6f1ea600efa83db963df80ae96
Yugang LIU <liuyug@gmail.com>
2020-10-30 16:03:38 +0800

    fix

--- py2exe.cmd
+++ py2exe.cmd
@@ -1,6 +1,19 @@
 
 rmdir build /s /q
 del dist\ndrop.exe /q
-pyinstaller ndrop.spec
 
-dist\ndrop.exe
+mkdir build
+
+set script=build\ndrop-script.py
+
+echo import ndrop.__main__ >> %script%
+echo ndrop.__main__.run() >> %script%
+
+pyinstaller ^
+--onefile ^
+--name ndrop ^
+--exclude-module tkinter ^
+--noconfirm ^
+%script%
+
+rem dist\ndrop.exe
