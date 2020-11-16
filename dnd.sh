0a11e6b95a8fa5a2a32a16aac1bd91f72c274d88
Yugang LIU <liuyug@gmail.com>
2020-11-16 16:25:41 +0800

    use curl to download

--- dnd.sh
+++ dnd.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 if [ ! -f tkinterdnd2-master.zip ]; then
-    wget https://github.com/pmgagne/tkinterdnd2/archive/master.zip -O tkinterdnd2-master.zip
+    # wget https://github.com/pmgagne/tkinterdnd2/archive/master.zip -O tkinterdnd2-master.zip
+    curl -L https://github.com/pmgagne/tkinterdnd2/archive/master.zip -o tkinterdnd2-master.zip
 fi
 
 rm -rf tkinterdnd2
